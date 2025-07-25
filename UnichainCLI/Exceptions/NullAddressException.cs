cb84aac260b592a550501b5bc4d1521f48a00329
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:44 -0700

    NullAddressException.cs

---
+++ UnichainCLI/Exceptions/NullAddressException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Unichain.Exceptions
+{
+    public class NullAddressException : Exception
+    {
+        public NullAddressException() : base("The address is null!")
+        {
+        }
+
+        public NullAddressException(string message) : base("The address is null!\nDescription: " + message)
+        {
+        }
+    }
+}
