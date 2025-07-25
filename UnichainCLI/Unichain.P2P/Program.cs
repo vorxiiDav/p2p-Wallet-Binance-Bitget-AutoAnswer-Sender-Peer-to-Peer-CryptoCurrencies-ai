3af09fd6554d515663bd6bfe78eb33959d2ae57a
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:22 -0700

    Program.cs

---
+++ UnichainCLI/Unichain.P2P/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Quic;
+using System.Security.Cryptography;
+using System.Text;
+using Unichain.P2P;
+
+
+//List<UnichainNode> nodes = [];
+
+//int bootnodePort = 1234;
+//const int nodeCount = 1;
+//var bootnode = new UnichainNode(bootnodePort);
+//for(int i= 1; i <= nodeCount; i++) {
+//    var node = new UnichainNode(bootnodePort + i);
+//    nodes.Add(node);
+//}
+
+//bootnode.Start(null);
+
+//Parallel.ForEach(nodes, node => {
+//    node.Start(bootnode.Address);
+//});
+//bootnode.Stop();
